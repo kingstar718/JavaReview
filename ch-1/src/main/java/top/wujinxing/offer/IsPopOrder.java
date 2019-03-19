01579e4c82a4c839dee7f2fa98db60e477412266
kingstar718 <936016045@qq.com>
2019-03-19 09:43:52 +0800

    IsPopOrderTest 栈的压入弹出序列

---
+++ ch-1/src/main/java/top/wujinxing/offer/IsPopOrder.java
@@ -0,0 +1,39 @@
+package top.wujinxing.offer;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+/**
+ * @author: wujinxing
+ * @date: 2019/3/19 09:17
+ * @description: 栈的压入、弹出序列
+ * 输入两个整数序列，第一个序列表示栈的压入顺序，
+ * 请判断第二个序列是否可能为该栈的弹出顺序。假设压入栈的所有数字均不相等。
+ * 例如序列1,2,3,4,5是某栈的压入顺序，序列4,5,3,2,1是该压栈序列对应的一个弹出序列
+ * 但4,3,5,1,2就不可能是该压栈序列的弹出序列。（注意：这两个序列的长度是相等的）
+ */
+public class IsPopOrder {
+    public boolean IsPopOrderTest(int [] pushA,int [] popA) {
+        if(pushA.length == 0 || popA.length == 0){
+            return false;
+        }
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0,j=0; i<pushA.length;){
+            stack.push(pushA[i++]);
+            while(j<popA.length&&stack.peek()==popA[j]){
+                stack.pop();
+                j++;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    @Test
+    public void test(){
+        int[] a = {1,2,3,4,5};
+        int[] b = {4,5,3,2,1};
+        System.out.println(IsPopOrderTest(a,b));
+    }
+}
