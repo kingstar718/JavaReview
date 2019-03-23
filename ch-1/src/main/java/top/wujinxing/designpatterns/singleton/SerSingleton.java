609f752883461fa1a2c0d5f3cc2c3a0ab5d3d308
kingstar718 <936016045@qq.com>
2019-03-23 22:45:05 +0800

    Singleton

---
+++ ch-1/src/main/java/top/wujinxing/designpatterns/singleton/SerSingleton.java
@@ -0,0 +1,25 @@
+package top.wujinxing.designpatterns.singleton;
+
+/**
+ * @author: wujinxing
+ * @date: 2019/3/23 22:41
+ * @description:
+ */
+public class SerSingleton implements java.io.Serializable{
+    String name;
+
+    private SerSingleton(){
+        System.out.println("Singleton is create");
+        name = "SerSingleton";
+    }
+    private static SerSingleton instance = new SerSingleton();
+    public static SerSingleton getInstance(){
+        return instance;
+    }
+    public static void createString(){
+        System.out.println("createString in Singleton");
+    }
+    private Object readResolve(){   //阻止生成新的实例,总是返回当前对象
+        return instance;
+    }
+}
